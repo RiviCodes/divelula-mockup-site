9b1a7ad43c5bc6cdb732d1ec69014637be62a088
RiviCodes <jose.riveramz@udlap.mx>
2022-07-01 18:17:50 -0500

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        btnBlue: "#3164ad",
+        btnLime: "#b3c940",
+      },
+    },
   },
   plugins: [],
 };
