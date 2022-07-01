2ba7e22bbc38e1d0927ddb6867f238736fb43d6f
RiviCodes <jose.riveramz@udlap.mx>
2022-07-01 18:29:42 -0500

    Add style classes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,36 +2,36 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <nav className="flex flex-row items-center py-2 bg-slate-300">
+    <nav className="flex flex-row items-center py-2">
       <img
-        className="mr-auto xl:w-40 xl:p-4"
+        className="mr-auto xl:w-40 xl:p-3 xl:ml-3"
         src="images/GEM_logo.png"
         alt=""
       />
       <ul className="flex flex-row justify-center">
-        <li className="mr-6 text-sm font-semibold">
+        <li className="mr-6 text-sm font-semibold hover:text-customLime transition ease-in-out delay">
           <a href="">HOME</a>
         </li>
-        <li className="mr-6 text-sm font-semibold">
+        <li className="mr-6 text-sm font-semibold hover:text-customLime transition ease-in-out delay">
           <a href="">ABOUT US</a>
         </li>
-        <li className="mr-6 text-sm font-semibold">
+        <li className="mr-6 text-sm font-semibold hover:text-customLime transition ease-in-out delay">
           <a href="">GSS</a>
         </li>
-        <li className="mr-6 text-sm font-semibold">
+        <li className="mr-6 text-sm font-semibold hover:text-customLime transition ease-in-out delay">
           <a href="">GO</a>
         </li>
-        <li className="mr-6 text-sm font-semibold">
+        <li className="mr-6 text-sm font-semibold hover:text-customLime transition ease-in-out delay">
           <a href="">GIVE</a>
         </li>
-        <li className="mr-3 text-sm font-semibold">
+        <li className="mr-3 text-sm font-semibold hover:text-customLime transition ease-in-out delay">
           <a href="">CONTACT</a>
         </li>
       </ul>
-      <button className="btn mx-2 px-6 py-1 rounded-full text-sm font-semibold text-white bg-btnBlue">
+      <button className="btn mx-2 px-6 py-1 rounded-full text-sm font-semibold text-white bg-btnBlue hover:bg-customBlueHover transition ease-in-out delay">
         GIVE NOW
       </button>
-      <button className="btn mx-2 px-6 py-1 rounded-full text-sm font-semibold text-white bg-btnLime">
+      <button className="btn mx-2 px-6 py-1 rounded-full text-sm font-semibold text-white bg-customLime hover:bg-customLimeHover transition ease-in-out delay">
         {" "}
         APPLY NOW
       </button>
