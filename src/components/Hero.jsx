a1e8fdc47a080cbf779b525cd29d48c48bc52039
RiviCodes <jose.riveramz@udlap.mx>
2022-07-01 22:06:29 -0500

    Add responsive styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,10 +3,14 @@ import React from "react";
 export default function Hero() {
   return (
     <section
-      className="flex flex-col justify-center items-center h-96 w-auto md:h-screen bg-contain xl:bg-contain bg-no-repeat bg-[url('../public/images/bg.png')]"
+      className="flex flex-col justify-start xl:justify-center items-center h-96 w-auto md:h-screen bg-contain xl:bg-contain bg-no-repeat bg-[url('../public/images/bg.png')]"
       id="#hero"
     >
-      <img className="h-auto xl:w-20 xl:pb-24" src="images/play.png" alt="" />
+      <img
+        className="h-auto w-2/12 xl:w-20 pt-10 sm:pt-0 pb-2 sm:pb-0 xl:pb-24"
+        src="images/play.png"
+        alt=""
+      />
       <h3 className="self-end xl:mr-10 text-4xl text-white">
         CALLED TO <span className="font-extrabold">GO</span>
       </h3>
