7f1fd88a6f2bfb37b1f2a604efa7b339a96aebfa
RiviCodes <jose.riveramz@udlap.mx>
2022-07-01 21:13:18 -0500

    Add styling

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,9 +4,9 @@ export default function Intro() {
   return (
     <section className="flex flex-col items-center">
       {/* HEADER */}
-      <div className="flex flex-row items-center justify-center">
+      <div className="flex flex-row items-center justify-center mb-14">
         <img className="h-12" src="images/l-bracket.png" alt="" />
-        <p className="font-medium w-12/12 mx-14">
+        <p className="text-xl font-medium w-12/12 mx-14">
           Our desire is to expose communities all across the world<br></br> to
           the gospel by starting{" "}
           <span className="font-extrabold text-customLime">
@@ -17,7 +17,18 @@ export default function Intro() {
       </div>
 
       {/* GRID */}
-      <div className="grid grid-cols-1 xl:grid-cols-12"></div>
+      <div className="grid grid-cols-1 items-center xl:grid-cols-12">
+        <div className="col-span-6 justify-self-end mr-32">
+          <h3 className="text-customBlue text-2xl tracking-wider">
+            Planting Seeds<br></br> of the{" "}
+            <span className="font-extrabold tracking-normal">GOSPEL</span>
+          </h3>
+          <p className="text-sm text-customBlue">ONE CLASSROOM AT A TIME</p>
+        </div>
+        <div className="col-span-6">
+          <img src="images/map.png" alt="" />
+        </div>
+      </div>
     </section>
   );
 }
