c7d6f6674bad2f4d39f54f85a711d8a8a9001ba8
RiviCodes <jose.riveramz@udlap.mx>
2022-07-01 20:12:21 -0500

    Add header

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function Intro() {
-  return <></>;
+  return (
+    <section className="flex flex-col items-center">
+      <p className="font-medium">
+        Our desire is to expose communities all across the world to the gospel
+        by starting{" "}
+        <h2 className="font-extrabold text-customLime">
+          GOSPEL SATURATED SCHOOLS
+        </h2>
+      </p>
+    </section>
+  );
 }
