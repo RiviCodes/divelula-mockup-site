d5a51e1aadfc1b56c972a9c6b6a3a5c9a64d407f
RiviCodes <jose.riveramz@udlap.mx>
2022-07-01 23:03:25 -0500

    Add responsive margin

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,11 +3,11 @@ import React from "react";
 export default function Hero() {
   return (
     <section
-      className="flex flex-col justify-start xl:justify-center items-center h-auto xl:h-96 w-auto md:h-screen bg-contain xl:bg-cover bg-no-repeat bg-[url('../public/images/bg.png')]"
+      className="flex flex-col justify-start xl:justify-center items-center h-40 xl:h-96 w-auto md:h-screen bg-cover xl:bg-cover bg-no-repeat bg-[url('../public/images/bg.png')]"
       id="#hero"
     >
       <img
-        className="h-auto w-2/12 xl:w-20 mt-28 sm:pt-0 pb-2 sm:pb-0 xl:pb-24"
+        className="h-auto w-1/12 xl:w-20 mt-14 xl:mt-28 sm:pt-0 pb-4 sm:pb-0 xl:pb-24"
         src="images/play.png"
         alt=""
       />
