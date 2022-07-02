a5a85fbb346ecb2fe1a1e19102cbe9d51251d042
RiviCodes <jose.riveramz@udlap.mx>
2022-07-01 20:53:06 -0500

    Add styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero() {
       className="flex flex-col justify-center items-center h-96 w-auto md:h-screen bg-contain xl:bg-contain bg-no-repeat bg-[url('../public/images/bg.png')]"
       id="#hero"
     >
-      <img className="h-auto xl:w-20 xl:mb-24" src="images/play.png" alt="" />
+      <img className="h-auto xl:w-20 xl:pb-24" src="images/play.png" alt="" />
       <h3 className="self-end xl:mr-10 text-4xl text-white">
         CALLED TO <span className="font-extrabold">GO</span>
       </h3>
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,13 +3,18 @@ import React from "react";
 export default function Intro() {
   return (
     <section className="flex flex-col items-center">
-      <p className="font-medium">
-        Our desire is to expose communities all across the world to the gospel
-        by starting{" "}
-        <h2 className="font-extrabold text-customLime">
-          GOSPEL SATURATED SCHOOLS
-        </h2>
-      </p>
+      {/* HEADER */}
+      <div className="flex flex-row items-center justify-center">
+        <img className="h-12" src="images/l-bracket.png" alt="" />
+        <p className="font-medium w-12/12 mx-14">
+          Our desire is to expose communities all across the world<br></br> to
+          the gospel by starting{" "}
+          <span className="font-extrabold text-customLime">
+            GOSPEL SATURATED SCHOOLS
+          </span>
+        </p>
+        <img className="h-12" src="images/r-bracket.png" alt="" />
+      </div>
     </section>
   );
 }
