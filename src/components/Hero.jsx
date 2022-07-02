21185fb2fc93b9cc64e8b496839b6f3f23a4276e
RiviCodes <jose.riveramz@udlap.mx>
2022-07-02 00:05:45 -0500

    Edit margin, padding

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,11 +3,11 @@ import React from "react";
 export default function Hero() {
   return (
     <section
-      className="flex flex-col justify-start xl:justify-center items-center h-40 md:h-96 xl:h-96 w-auto md:w-auto bg-center bg-cover md:bg-cover xl:bg-cover bg-no-repeat bg-[url('../public/images/bg.png')]"
+      className="flex flex-col justify-start xl:justify-center items-center h-48 md:h-96 xl:h-96 w-auto md:w-auto bg-center bg-cover md:bg-cover xl:bg-cover bg-no-repeat bg-[url('../public/images/bg.png')]"
       id="#hero"
     >
       <img
-        className="h-auto w-1/12 xl:w-20 mt-14 xl:mt-28 sm:pt-0 pb-4 sm:pb-0 xl:pb-24"
+        className="h-auto w-1/12 xl:w-20 mt-16 xl:mt-28 sm:pt-0 pb-10 sm:pb-0 xl:pb-24"
         src="images/play.png"
         alt=""
       />
