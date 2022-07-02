36077cd85d820d7812acc60659ef2117bdc8b058
RiviCodes <jose.riveramz@udlap.mx>
2022-07-01 22:21:06 -0500

    Fix margin

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,11 +3,11 @@ import React from "react";
 export default function Hero() {
   return (
     <section
-      className="flex flex-col justify-start xl:justify-center items-center h-96 w-auto md:h-screen bg-contain xl:bg-contain bg-no-repeat bg-[url('../public/images/bg.png')]"
+      className="flex flex-col justify-start xl:justify-center items-center h-auto xl:h-96 w-auto md:h-screen bg-contain xl:bg-cover bg-no-repeat bg-[url('../public/images/bg.png')]"
       id="#hero"
     >
       <img
-        className="h-auto w-2/12 xl:w-20 pt-10 sm:pt-0 pb-2 sm:pb-0 xl:pb-24"
+        className="h-auto w-2/12 xl:w-20 mt-28 sm:pt-0 pb-2 sm:pb-0 xl:pb-24"
         src="images/play.png"
         alt=""
       />
